a0bac183cf5140305efb43056f81b85408d37a1b
yokaimeow <yokaimeow@qq.com>
2021-02-10 21:46:35 +0800

    update app page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h1>
+        <span><i>Welcome</i></span>
+        <span><i>to</i></span>
+        <span><i>use</i></span>
+        <span><i>our</i></span>
+        <span><i>products</i></span>
+      </h1>
+      <p>
+        Subscribe, Like, Comment and Share.
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
